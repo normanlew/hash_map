bb2d386d1d57c706446e5794e9e868849c915ad7
Norman Lew <normanlew@users.noreply.github.com>
2026-07-14 14:56:00 -0700

    Implement set, hash and entries functions

---
+++ hashmap.js
@@ -0,0 +1,87 @@
+class HashMap {
+    load_factor = .75;
+    capacity = 16;
+    size = 0;
+    h_map = new Array(this.capacity).fill(new Node());
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+
+        return hashCode;
+    } 
+
+    set(key, value) {
+        let hashCode = this.hash(key);
+        // console.log("hash code: " + hashCode);
+        let node = this.h_map[hashCode];
+        let key_exists = false;
+
+        while (node.nextNode != null) {
+            node = node.nextNode;
+            if (node.value.key === key) {
+                key_exists = true;
+                node.value.value = value;
+                break;
+            }
+        }
+
+        if (!key_exists) {
+            let new_node = new Node();
+            let new_entry = {"key": key, "value": value};
+            new_node.value = new_entry;
+            node.nextNode = new_node;
+            this.size++;
+
+            if (this.size > (this.load_factor * this.capacity)) {
+                expand_map_array();
+            }
+        }
+    }
+// takes two arguments: the first is a key, and the second is a value that is assigned 
+// to this key. If a key already exists, then the old value is overwritten, and we 
+// can say that we update the key’s value (e.g. Carlos is our key but it is called 
+//     twice: once with value I am the old value., and once with value I am the new value.. 
+//     Following this logic, Carlos should contain only the latter value).
+
+// Recall that collisions occur when TWO DIFFERENT keys generate the same hash code 
+// and get assigned to the same bucket. (e.g. Rama and Sita are both hashed to 3, 
+//     so 3 becomes a location for Rama AND Sita. However, we know that this is
+//  not an update because the keys are different). Review the dealing with collisions 
+//  section of the previous lesson to find a way to handle our collisions.
+
+// Remember to grow your buckets to double their capacity when your hash map reaches 
+// the load factor. The methods mentioned later in this assignment can help you 
+// handle the growth logic, so you may want to implement this feature near the end. 
+// However, we mention this with set() because it’s important to grow buckets exactly 
+// as they are being expanded.
+
+    entries() {
+        let return_array = [];
+        for (let i = 0; i < this.h_map.length; i++) {
+            let node = this.h_map[i];
+            while (node.nextNode != null) {
+                node = node.nextNode;
+                // console.log("pushing: " + node.value);
+                return_array.push([node.value.key, node.value.value]);
+            }
+        }
+        return return_array;
+    }
+
+    expand_map_array() {
+
+    }
+
+}
+
+class Node {
+    value = null;
+    nextNode = null;
+}
+
+export{HashMap};
